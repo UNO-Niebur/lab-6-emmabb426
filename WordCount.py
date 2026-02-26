30779bb80f5a686b78febe3aa430cd474022a2b1
emmabb426 <ebarnes17@unomaha.edu>
2026-02-26 20:21:36 +0000

    WordIndex and WordCount done

--- WordCount.py
+++ WordCount.py
@@ -4,7 +4,7 @@
 #Assignment: Word Count
 
 def main():
-  #file_name = input("File (.txt) to open: ")
+  
   while True:
     file_name = input("File (.txt) to open: ")
     try:
@@ -32,6 +32,7 @@ def main():
 
     except FileNotFoundError:
       print("Error: The file could not be found. Please try again.")
+
     except (KeyboardInterrupt, SystemExit):
       print("Exiting program.")
       break
--- WordIndex.py
+++ WordIndex.py
@@ -4,17 +4,41 @@
 #Assignment:
 
 def main():
-  textFile = open("fish.txt", 'r')
-  
-  words = {} #create an empty dictionary
-  
-  
-  print ("fish" in words) #is a word already in the dictionary?
-  words["fish"] = [2]     #add a list to the dictionary
-  print ("fish" in words) #is the word there now?
-  words["fish"].append(5) #add to an existing list
-  print(words)
+  while True:
+    file_name = input("File (.txt) to index: ")
+    #textFile = open("fish.txt", 'r')
+    try:
+      with open(file_name, 'r') as file:
+    
+        words = {} #create an empty dictionary
+        lineNum = 0
+        
 
+        for line in file:
+          lineNum = lineNum + 1
+          wordList = line.replace('—' , ' ').split()
+          for w in wordList:
+            w = w.lower()
+            w = w.replace("," , "")
+            w = w.replace("." , "")
+            w = w.replace("!" , "")
+            #w = w.replace("—" , " ")
+            
+            if w in words:
+              if lineNum not in words[w]:
+                words[w].append(lineNum)
+            else:
+              words[w] = [lineNum]
+        
+        for word in words:
+          print(word, words[word])
+      break
+    except FileNotFoundError:
+      print("Error: The file could not be found. Please try again.")
+
+    except (KeyboardInterrupt, SystemExit):
+      print("Exiting program.")
+      break
 
 if __name__ == '__main__':
   main()
