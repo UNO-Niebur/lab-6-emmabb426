4a76b51a837e38499b4e649cd80912f563327137
emmabb426 <ebarnes17@unomaha.edu>
2026-03-12 16:05:04 +0000

    edit sum (2-12)

--- DiceRoll.py
+++ DiceRoll.py
@@ -6,7 +6,7 @@ import random
 
 def main():
   #Create an empty list with possible roll values
-  rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
+  rolls = [0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
   number_rolls = int(input("How many rolls do you want to simulate? "))
   for r in range(number_rolls):
@@ -15,9 +15,9 @@ def main():
 
     #find the sum total of the two dice
     sum = dice1 + dice2
-    rolls[sum - 1] = rolls[sum -1] +1
+    rolls[sum - 2] = rolls[sum -2] +1
   #print statictics for dice rolls
-  dice = 1
+  dice = 2
   for count in rolls:
     print(dice, ":", count, ":", round(count/number_rolls*100, 2), "%")
     dice = dice + 1
